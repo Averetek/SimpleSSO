2dd751871b7c0289e9fbbe6ecf3ea22b172023d2
John Migas <john@averetek.com>
2016-07-29 17:17:36 -0700

    Enhancing error reporting and adding test functionality.

--- SimpleSSO/SSOIn.aspx.cs
+++ SimpleSSO/SSOIn.aspx.cs
@@ -7,8 +7,8 @@ namespace SimpleSSO
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			if (IsPostBack) return;
-
-			var userRole = Request.Form["userRole"].Trim();
+            #region Read Form
+            var userRole = Request.Form["userRole"].Trim();
 			var userId = Request.Form["userId"].Trim();
 			var email = (Request.Form["email"] ?? string.Empty).Trim();
 			var first = (Request.Form["first"] ?? string.Empty).Trim();
@@ -27,8 +27,51 @@ namespace SimpleSSO
 			var sharedKey = System.Configuration.ConfigurationManager.AppSettings["SharedKey"];
 			var hashInfo = string.Format("{0}{1}{2}", organizationNumber, email, sharedKey);
 			var hash = CalculateSha1(hashInfo);
+            var hashStatus = hash.ToLower().Equals(v.ToLower());
+            #endregion 
 
-			ltUserRole.Text = userRole;
+            /* This is here for testing purposes only. Remove after a successful setup. */
+            #region Validation
+            var regexUtils = new Library.RegexUtilities();
+            if(regexUtils.IsValidEmail(email))
+            {
+                Response.Write("ERROR: Missing or invalid email address");
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(country))
+            {
+                Response.Write("ERROR: Missing country");
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(userId))
+            {
+                Response.Write("ERROR: Missing userId");
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(organizationNumber))
+            {
+                Response.Write("ERROR: Missing organizationNumber");
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(organizationName))
+            {
+                Response.Write("ERROR: Missing organizationName");
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(v))
+            {
+                Response.Write("ERROR: Missing v e.g. SHA1(organizationNumber + email + sharedKey)");
+                return;
+            }
+            if(!hashStatus)
+            {
+                Response.Write($"ERROR: Invalid verification code (v). Expecting {hash}");
+                return;
+            }
+            #endregion
+
+            #region Output
+            ltUserRole.Text = userRole;
 			ltUserID.Text = userId;
 			ltEmail.Text = email;
 			ltFistName.Text = first;
@@ -45,8 +88,8 @@ namespace SimpleSSO
 
 			ltHashStatus.Text = hash.ToLower().Equals(v.ToLower()) ? "True" : "False";
 			ltHash.Text = hash.ToLower().Equals(v.ToLower()) ? hash : hash + " != " + v;
-
-		}
+            #endregion
+        }
 
 		private static string CalculateSha1(string text)
 		{
